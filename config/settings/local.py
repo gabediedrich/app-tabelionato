8c8b5f82ad461c0bda549313e409220717f59edd
gabediedrich <73556513+gabediedrich@users.noreply.github.com>
2021-01-21 03:24:32 -0300

    heroku config

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="cZb7pOcarATrLIKmVbxQ82JebnwhJdUtXwm8VkwFA5S5PWwlkKUrmFtkIZFTmtSJ",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "app-tabelionato.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
