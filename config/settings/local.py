a7c9fd28b272706bff7d71cb925cdb89f1abba18
gabediedrich <73556513+gabediedrich@users.noreply.github.com>
2021-01-22 20:22:41 -0300

    database connection

--- config/settings/local.py
+++ config/settings/local.py
@@ -25,7 +25,7 @@ CACHES = {
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'app_tabelionato',
@@ -34,7 +34,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
-}
+}"""
 
 # EMAIL
 # ------------------------------------------------------------------------------
